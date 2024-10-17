ca165678051d50a2ea881873614b14fcea53800b
Aryan0527 <aryananurag8@outlook.com>
2024-10-17 16:49:07 +0530

    users page

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,13 +1,63 @@
-import React from 'react'
-import styles from '../../../ui/dashboard/users/users.module.css';
+import React from "react";
+import styles from "../../../ui/dashboard/users/users.module.css";
+import Search from "@/ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
 const Users = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        
+        <Search placeholder="Search for users..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
       </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Jhonny Depp
+              </div>
+            </td>
+            <td>depp@gmail.com</td>
+            <td>27.07.2022</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
